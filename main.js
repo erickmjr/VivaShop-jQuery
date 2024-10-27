bf4c24028d90d66633dfc514cb637d46e76a99cc
erickmjr <erickmellojr@gmail.com>
2024-10-27 15:13:47 -0300

    Adicionando carrosel de imagens + melhorias na responsividade

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
+    $("#carousel-images").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:  false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+        
+    })
 
     $(".menu-hamburguer").click(function(){
         $("nav").slideToggle();
